91633b081502f83586c596120cf36fc45ea7bb13
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2023-11-18 15:37:03 +0000

    Deploying to gh-pages from @ softuza/portal@909db9bb1ef771fc7e7d2354aee6b8a3d6783f89 🚀

---
+++ en-US/chunk-LFZD73XQ.js
@@ -0,0 +1,1 @@
+var e={tests:"Tests",testClass:"Test Class",testGroup:"Test Group",testResult:"Test Result",referenceGroup:"Reference Group",referenceValue:"Reference Value"};export{e as a};/**i18n:33360f080214f876987bd0ecc2f51b8308b133d904c534ef8c974004cf54d934*/
