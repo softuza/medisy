49ada04e7397a8971839f480759943a5c567645c
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2023-11-19 12:23:41 +0000

    Deploying to gh-pages from @ softuza/portal@bc86d62ae0f1585e1c9759c81105d89301bc3c99 🚀

--- en-US/chunk-5MX2FBOB.js
+++
@@ -1,1 +0,0 @@
-var e={id:"Id",name:"Name",username:"Username",description:"Description",updatedDate:"Updated Date",createdDate:"Created Date",code:"Code",enabled:"Enabled",dashboard:"Dashboard",users:"Users",user:"User",persons:"Persons",notifications:"Notifications",reports:"Reports"};export{e as a};/**i18n:419148c294ae56579e504a43f00a8e71eb3633c9912a0864afd0e0662e47e985*/
