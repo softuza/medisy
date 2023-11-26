607f5c73655a91b7d17a2f0950916571389259fd
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2023-11-26 23:47:17 +0000

    Deploying to gh-pages from @ softuza/portal@9ffdcbbdcd3b3250e90643586cc4fa06497373a1 🚀

--- ar-SY/chunk-3LBYHEZ3.js
+++
@@ -1,2 +0,0 @@
-var r=class{static default(e,p){return{pageSize:p??50,pageIndex:e??1}}static unpaged(){return{pageSize:-1,pageIndex:1}}static count(){return{pageIndex:1,pageSize:0}}};var t=class{static default(e){return{query:e}}};export{r as a,t as b};
-/**i18n:5ebc65b24f90b78d556a3a8c188ab6405e20b5534cf6eb00a5dd3e44f73f97d4*/
