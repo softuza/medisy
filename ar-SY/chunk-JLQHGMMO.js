65886348a8702aa357db4f823cc58f51f0afe94a
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-03-04 19:48:41 +0000

    Deploying to gh-pages from @ softuza/portal@f4b5795c5164d8c00ab385073d4927f68af2dcb3 🚀

---
+++ ar-SY/chunk-JLQHGMMO.js
@@ -0,0 +1,2 @@
+var o=function(r){return r.NationalCard="NationalCard",r.BankCard="BankCard",r.DriverLicense="DriverLicense",r.Passport="Passport",r.TravelDocument="TravelDocument",r.Other="Other",r}(o||{});var a=function(r){return r.Male="Male",r.Female="Female",r}(a||{});export{o as a,a as b};
+/**i18n:16c9224f5b668b40e6178402b9ae5cba52679b665b9b111ce72150c0a38fe8d5*/
