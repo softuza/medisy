2f86eb27ac7315b5fa8feb6ca0f52b54053ef882
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-03-10 12:00:29 +0000

    Deploying to gh-pages from @ softuza/portal@f86c291e5d3fb827784aae5ef80ec99ffc80197f 🚀

---
+++ ar-SY/chunk-HQBETVMD.js
@@ -0,0 +1,2 @@
+import{f}from"./chunk-NLKNVIYO.js";function s(i){for(let r in i)if((i[r]===void 0||i[r]===null)&&delete i[r],i[r]instanceof Date)i[r]=i[r].toISOString();else if(r=="filters"&&i[r]instanceof Array){let n=[];i[r].forEach(e=>{typeof e=="string"?n.push(e):"field"in e&&"op"in e&&"value"in e&&n.push(f.serialize(e))}),i[r]=n}return i}export{s as a};
+/**i18n:0f14478d9f76834c336477492adead1f84c567d512dc7742568a2120e6a13b69*/
