915ae7a7ebf5a9396bc81c966e4c6d495264b193
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-03-27 23:37:31 +0000

    Deploying to gh-pages from @ softuza/portal@0aecbab463034cbca59594d4e90c98e765181c55 🚀

--- en-US/chunk-BWOMI5H5.js
+++
@@ -1,2 +0,0 @@
-import{b as s,c as a}from"./chunk-3RFVV3LE.js";import{s as n}from"./chunk-ZTPR7VOL.js";import{Ia as r,ya as o}from"./chunk-3ZQQMLH6.js";var l=(()=>{let t=class t extends a{constructor(e){super(e,"softuza-notification"),this.notifications=new s(this.http,`${this.basePath}/v1/notifications`),this.messages=new s(this.http,`${this.basePath}/v1/messages`)}};t.\u0275fac=function(c){return new(c||t)(r(n))},t.\u0275prov=o({token:t,factory:t.\u0275fac});let i=t;return i})();export{l as a};
-/**i18n:34a0aca9a8a7a5b54ef908b3a50021d42a3fb6a7b58cfe4980f9bcbab369501e*/
