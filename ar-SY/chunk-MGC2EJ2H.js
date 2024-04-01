dae75036bae71948a6cf3a19904f4d3ce8c2b46f
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-04-01 20:18:19 +0000

    Deploying to gh-pages from @ softuza/portal@7212aca8daf9eeb50597bac8da396507a7770257 🚀

--- ar-SY/chunk-MGC2EJ2H.js
+++
@@ -1,2 +0,0 @@
-import{a}from"./chunk-CFWDSB5H.js";import"./chunk-M5SMZ234.js";import"./chunk-EIB4LKCD.js";import"./chunk-NXFEVB6F.js";export{a as SzTableStateComponent};
-/**i18n:16b060cd78820ad434cadcf06c146ea03505a76210f247eb6a3abe146386bf6b*/
