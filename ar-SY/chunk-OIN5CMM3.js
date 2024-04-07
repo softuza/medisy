8e3200332f7a71ef189280b69d4873fff36ddac7
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-04-07 21:58:19 +0000

    Deploying to gh-pages from @ softuza/portal@5b7295c6586d72fdbec1802ad01406982b577fb0 🚀

---
+++ ar-SY/chunk-OIN5CMM3.js
@@ -0,0 +1,2 @@
+var t=(()=>{let s=class s{};s.Notifications="snti/notifications",s.NotificationEmails="snti/notification-emails",s.NotificationSmses="snti/notification-smses",s.NotificationPushes="snti/notification-pushes",s.Messages="snti/messages",s.PushSubscription="snti/push-subscriptions";let i=s;return i})();export{t as a};
+/**i18n:7992b670fa912c61e7fcc3b33bbd0ab051bcb5cc240f907263ef623fbdc93e3b*/
