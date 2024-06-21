4f23cf5846f3e9d41efe0980e14ce8ea3a1a1e48
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-06-21 17:02:44 +0000

    Deploying to gh-pages from @ softuza/portal@c9eead6ebe84dada963adb1d5cf0369ee27bfaca 🚀

--- ar-SY/chunk-MGLJOJVE.js
+++
@@ -1,2 +0,0 @@
-import{e}from"./chunk-HMSPALCE.js";import"./chunk-GSP5ENK2.js";import{ea as r,oa as i}from"./chunk-MR5DLTC3.js";import"./chunk-XTPTFZB3.js";var n=[{path:"login",loadComponent:()=>import("./chunk-6HVHLVZ5.js").then(o=>o.SzAuthLoginComponent)}];var d=(()=>{let t=class t{};t.\u0275fac=function(m){return new(m||t)},t.\u0275mod=i({type:t}),t.\u0275inj=r({imports:[e.forChild(n)]});let o=t;return o})();export{d as AuthModule};
-/**i18n:2cc98dd3a3b5bc180bc78b3f1af50b699468d67591cd411b77ad1068c18b35e2*/
