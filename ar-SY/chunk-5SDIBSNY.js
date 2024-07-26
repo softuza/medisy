2bb8bd3cd2307834be126d7d4e413684d64019b3
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-07-26 19:44:28 +0000

    Deploying to gh-pages from @ softuza/portal@9a7a8ce2274c247b815f9e1150d90202d8bb570f 🚀

--- ar-SY/chunk-5SDIBSNY.js
+++
@@ -1,2 +0,0 @@
-var t=(()=>{let c=class c{};c.Projects="cstn/projects",c.Project="cstn/projects/:projectId",c.Zones="cstn/zones",c.Workers="cstn/workers",c.WorkGroups="cstn/work-groups",c.Equipment="cstn/equipment";let s=c;return s})();export{t as a};
-/**i18n:2203e027d52d3f0736b00c23c47b63f6931e6ad93b70d88f30a525b6a40c92e8*/
