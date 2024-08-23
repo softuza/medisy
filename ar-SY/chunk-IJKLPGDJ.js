21a738e0388c6007a4d7dc8bf515a77699c69e1d
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-08-23 05:48:28 +0000

    Deploying to gh-pages from @ softuza/portal@9de1714463eafdde5d0823386c6b278cdf8be742 🚀

---
+++ ar-SY/chunk-IJKLPGDJ.js
@@ -0,0 +1,2 @@
+var e=(()=>{let t=class t{};t.Templates="srpt/templates",t.Template="srpt/templates/:templateId",t.Printers="srpt/printers";let s=t;return s})();export{e as a};
+/**i18n:23b18f90dfad26ae6b830c3e760ecc323aedcfc43eb78549f6a00c2171dce7c8*/
