c78eea9a88780ab22e8f7cb6992b544daf6245fd
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-10-06 06:23:39 +0000

    Deploying to gh-pages from @ softuza/portal@4ce17b9461dcd9d531394f96f943c064b77a5dbd 🚀

---
+++ en-US/chunk-6NA5VWKS.js
@@ -0,0 +1,2 @@
+var u=class{static object(){return e=>e.value&&typeof e.value!="object"?{invalid:{value:e.value}}:null}static startsWith(e){return t=>!e||!t.value?null:typeof t.value!="string"||!t.value.startsWith(e)?{invalid:{value:t.value}}:null}};export{u as a};
+/**i18n:90fd3a02d1f38af464e16e15bd62cf2b8c3351a95846e9a75ddd118742a5a7ba*/
