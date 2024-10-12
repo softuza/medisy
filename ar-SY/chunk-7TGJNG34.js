0707600dc224b7470385f4d14badabebebe16529
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-10-12 18:31:53 +0000

    Deploying to gh-pages from @ softuza/portal@a9445b0aa1d185399c46a9afde8e01158db1f609 🚀

--- ar-SY/chunk-7TGJNG34.js
+++
@@ -1,2 +0,0 @@
-import"./chunk-ZA5F7ID5.js";var t=[{path:"login",loadComponent:()=>import("./chunk-O4LYTXFZ.js").then(o=>o.SzAuthLoginComponent)}];export{t as default};
-/**i18n:9054193bb4dcc24d51a6dba534830e37dcd91e77a8a9ecb6b346a792eb872c67*/
---
+++ ar-SY/chunk-UBK4IOTP.js
@@ -0,0 +1,2 @@
+import"./chunk-ZA5F7ID5.js";var t=[{path:"login",loadComponent:()=>import("./chunk-7A33J546.js").then(o=>o.SzAuthLoginComponent)}];export{t as default};
+/**i18n:9054193bb4dcc24d51a6dba534830e37dcd91e77a8a9ecb6b346a792eb872c67*/
