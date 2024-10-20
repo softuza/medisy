79c05e347be7465aab3101c9f1af3b26254b5ee5
osamaz26 <26938312+osamaz26@users.noreply.github.com>
2024-10-20 12:07:15 +0000

    Deploying to gh-pages from @ softuza/portal@508848d2b92550075772ab0f49e1c2beeac5a4e4 🚀

---
+++ ar-SY/chunk-3MSCRTEO.js
@@ -0,0 +1,2 @@
+import"./chunk-KKIINILB.js";var t=[{path:"login",loadComponent:()=>import("./chunk-WL4IF6H2.js").then(o=>o.SzAuthLoginComponent)}];export{t as default};
+/**i18n:52dae1015fb4dcd24d02f3b60578855a39e5cc5be32446a479305eae917c802c*/
